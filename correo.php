2532c94ccdc3aed221b34d97e7d6a300f33e9f73
Jparra-21 <javierparraguirrel@gmail.com>
2023-04-12 04:49:15 +0000

    Cambios para pagina

--- correo.php
+++ correo.php
@@ -1,20 +1,20 @@
 <?php
 if (isset($_POST['email'])) {
 
-    // EDIT THE FOLLOWING TWO LINES:
+    // Editar Las siguientes lineas
     $email_to = "javierparraguirrel@gmail.com";
     $email_subject = "Nuevo Contacto";
 
     function problem($error)
     {
-        echo "We're sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br><br>";
+        echo "Lo sentimos pero hay un problema(s) con su mensaje.";
+        echo "Estos rrores parecen acontinuacion.<br><br>";
         echo $error . "<br><br>";
-        echo "Please go back and fix these errors.<br><br>";
+        echo "Porfavor regrese y arregle estos problemas.<br><br>";
         die();
     }
 
-    // validation expected data exists
+    // Validacion que existan los datos
     if (
         !isset($_POST['nombre']) ||
         !isset($_POST['email']) ||
@@ -24,33 +24,33 @@ if (isset($_POST['email'])) {
         problem("Lo sentimos pero hay un problema con su mensaje.");
     }
 
-    $nombre = $_POST['nombre']; // required
-    $email = $_POST['email']; // required
-    $telefono = $_POST['telefono']; // required
-    $mensaje = $_POST['Message']; // required
+    $nombre = $_POST['nombre']; // requerido
+    $email = $_POST['email']; // requerido
+    $telefono = $_POST['telefono']; // requerido
+    $mensaje = $_POST['mensaje']; // requerido
 
     $error_mensaje = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
     if (!preg_match($email_exp, $email)) {
-        $error_mensaje .= 'The Email address you entered does not appear to be valid.<br>';
+        $error_mensaje .= 'El email que ingreso al parecer no es valido.<br>';
     }
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
-    if (!preg_match($string_exp, $name)) {
-        $error_mensaje .= 'The Name you entered does not appear to be valid.<br>';
+    if (!preg_match($string_exp, $nombre)) {
+        $error_mensaje .= 'El mensaje que ingreso no es valido.<br>';
     }
 
-    if (strlen($message) < 2) {
-        $error_mensaje .= 'The Message you entered do not appear to be valid.<br>';
+    if (strlen($mensaje) < 2) {
+        $error_mensaje .= 'EL mensaje que ingreso no es valido.<br>';
     }
 
     if (strlen($error_mensaje) > 0) {
         problem($error_mensaje);
     }
 
-    $email_mensaje = "Form details below.\n\n";
+    $email_mensaje = "Mensaje de contacto a continuacion:\n\n";
 
     function clean_string($string)
     {
@@ -58,12 +58,12 @@ if (isset($_POST['email'])) {
         return str_replace($bad, "", $string);
     }
 
-    $email_mensaje .= "Nombre: " . clean_string($name) . "\n";
+    $email_mensaje .= "Nombre: " . clean_string($nombre) . "\n";
     $email_mensaje .= "Email: " . clean_string($email) . "\n";
     $email_mensaje .= "Telefono: " . clean_string($telefono) . "\n";
     $email_mensaje .= "Mensaje: " . clean_string($mensaje) . "\n";
 
-    // create email headers
+    // Encabezado
     $headers = 'De: ' . $email . "\r\n" .
         'Responder-a: ' . $email . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
@@ -72,7 +72,6 @@ if (isset($_POST['email'])) {
 }
 ?>
 
-    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
 
     <html><body><p>Gracias por contactarnos.</p>
     <button onclick="history.go(-1);">Back </button></body></html>
